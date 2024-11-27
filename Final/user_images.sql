9c723ccd5fe1395f282b9e7275571465f7a3881a
MONISHA508 <148743811+MONISHA508@users.noreply.github.com>
2024-11-27 13:52:10 +0530

    Update user_images.sql

--- Final/user_images.sql
+++ Final/user_images.sql
@@ -1,1 +1,8 @@
-
+-- Create the table
+CREATE TABLE user_images (
+    id NUMBER PRIMARY KEY,
+    username VARCHAR2(255) NOT NULL,
+    image_data BLOB,
+    image_name VARCHAR2(255),
+    timestamp NUMBER
+);
